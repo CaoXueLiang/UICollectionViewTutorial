dfb137a97303221e3951e37783a44c20ec1f2afb
曹学亮 <2357747972@qq.com>
2017-11-06 18:44:13 +0800

    备份

--- StickyHeadersLayout/StickyHeadersLayout/Layout/StickyHeadersLayout.m
+++ StickyHeadersLayout/StickyHeadersLayout/Layout/StickyHeadersLayout.m
@@ -10,24 +10,30 @@
 
 @implementation StickyHeadersLayout
 #pragma mark - OverVide Menthod
-/*- (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect{
+- (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect{
+    
+    /*返回当前可视范围内元素的attributes,包括cells, supplementary views, 和 decoration views*/
     NSMutableArray *totalAttributeArray = [NSMutableArray arrayWithArray:[super layoutAttributesForElementsInRect:rect]];
     
     NSMutableIndexSet *Sections = [NSMutableIndexSet indexSet];
     for (UICollectionViewLayoutAttributes *attributes in totalAttributeArray) {
+        //找出当前cell对应的section索引
         if (attributes.representedElementCategory == UICollectionElementCategoryCell) {
             [Sections addIndex:attributes.indexPath.section];
         }
     }
+    
     for (UICollectionViewLayoutAttributes *attributes in totalAttributeArray) {
+        //遍历当前屏幕上显示的所有Header，然后将还显示在屏幕上的header的对应索引从Sections删除，这样就只保持了对刚刚移除屏幕header的追踪。
         if (attributes.representedElementKind == UICollectionElementKindSectionHeader) {
             [Sections removeIndex:attributes.indexPath.section];
         }
     }
     
+    /*将刚移出屏幕的 header (missing headers) 加入到 totalAttributeArray*/
     [Sections enumerateIndexesUsingBlock:^(NSUInteger idx, BOOL * _Nonnull stop) {
         NSIndexPath *path = [NSIndexPath indexPathForItem:0 inSection:idx];
-        UICollectionViewLayoutAttributes *layoutAttributes = [self layoutAttributesForItemAtIndexPath:path];
+        UICollectionViewLayoutAttributes *layoutAttributes = [self layoutAttributesForSupplementaryViewOfKind:UICollectionElementKindSectionHeader atIndexPath:path];
         [totalAttributeArray addObject:layoutAttributes];
     }];
     
@@ -73,9 +79,9 @@
 
 - (BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)newBounds{
     return YES;
-}*/
+}
 
-- (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect
+/*- (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect
 {
     NSMutableArray *answer = [[super layoutAttributesForElementsInRect:rect] mutableCopy];
     UICollectionView * const collectionView = self.collectionView;
@@ -92,7 +98,7 @@
             [missingSections removeIndex:layoutAttributes.indexPath.section];
         }
     }
-    
+ 
     [missingSections enumerateIndexesUsingBlock:^(NSUInteger idx, BOOL *stop) {
         
         NSIndexPath *indexPath = [NSIndexPath indexPathForItem:0 inSection:idx];
@@ -163,7 +169,7 @@
 - (BOOL) shouldInvalidateLayoutForBoundsChange:(CGRect)newBound
 {
     return YES;
-}
+}*/
 
 @end
 
